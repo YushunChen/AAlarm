963e5daf1bac97d6f7a2527c53a1f6128f7b7eb3
Mike Yang <zyang439@wisc.edu>
2021-10-19 01:53:17 -0500

    menu imitate lab3 m2

--- app/src/main/java/com/example/aalarm/MainActivity.java
+++ app/src/main/java/com/example/aalarm/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.aalarm;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    private NavigationBarView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnItemSelectedListener(bottomnavFunction);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new AlarmFragment()).commit();
+
+
     }
+
+    private NavigationBarView.OnItemSelectedListener bottomnavFunction = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.alarm:
+                    fragment = new AlarmFragment();
+                    break;
+                case R.id.plan:
+                    fragment = new PersonFragment();
+                    break;
+                case R.id.personal:
+                    fragment = new PlanFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+            return true;
+        }
+    };
 }
