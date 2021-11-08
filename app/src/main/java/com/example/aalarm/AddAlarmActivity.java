4e97ba41a127df3238da14def0c6c26744bae10d
YushunChen <56765896+YushunChen@users.noreply.github.com>
2021-11-08 17:15:49 -0600

    Created Alarm class and added valid inputs checks for adding a single alarm

--- app/src/main/java/com/example/aalarm/AddAlarmActivity.java
+++ app/src/main/java/com/example/aalarm/AddAlarmActivity.java
@@ -10,9 +10,7 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.os.Build;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
@@ -22,8 +20,11 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddAlarmActivity extends AppCompatActivity {
+
+    private static Alarm alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +48,14 @@ public class AddAlarmActivity extends AppCompatActivity {
         }
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            // Do something with the time chosen by the user
             Log.i("info", String.valueOf(hourOfDay));
             Log.i("info", String.valueOf(minute));
+            if (alarm != null) {
+                alarm.setHour(hourOfDay);
+                alarm.setMinute(minute);
+            } else {
+                alarm = new Alarm(hourOfDay, minute);
+            }
         }
     }
 
@@ -78,6 +84,13 @@ public class AddAlarmActivity extends AppCompatActivity {
             Log.i("info", String.valueOf(year));
             Log.i("info", String.valueOf(month));
             Log.i("info", String.valueOf(day));
+            if (alarm != null) {
+                alarm.setYear(year);
+                alarm.setMonth(month);
+                alarm.setDay(day);
+            } else {
+                alarm = new Alarm(year, month, day);
+            }
         }
     }
 
@@ -93,12 +106,45 @@ public class AddAlarmActivity extends AppCompatActivity {
 
     public void onUserAddAlarm(View v) {
 
-        // Set the alarm to start at approximately 2:00 p.m.
         Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 01);
-        calendar.set(Calendar.MINUTE, 9);
+        int year, month, day, hour, minute;
 
+        // check for empty user input and initialize alarm variables
+        if (alarm != null) {
+            year = alarm.getYear();
+            month = alarm.getMonth();
+            day = alarm.getDay();
+            hour = alarm.getHour();
+            minute = alarm.getMinute();
+        } else {
+            Toast.makeText(getApplicationContext(), "Please pick your date and time!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // check for full user input with both date and time
+        if (year != -1 && month != -1 && day != -1 && hour != -1 && minute != -1) {
+            calendar.setTimeInMillis(System.currentTimeMillis());
+            calendar.set(Calendar.YEAR, year);
+            calendar.set(Calendar.MONTH, month);
+            calendar.set(Calendar.DAY_OF_MONTH, day);
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+
+            // check for expired dates
+            Calendar currCalendar = Calendar.getInstance();
+            if (currCalendar.getTime().after(calendar.getTime())) {
+                Toast.makeText(getApplicationContext(), "You cannot live in the past!", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "Please pick your date and time!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        setUserAlarm(calendar);
+    }
+
+    private void setUserAlarm(Calendar calendar) {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
         Intent alarmIntent = new Intent(this, AlarmReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
@@ -113,7 +159,10 @@ public class AddAlarmActivity extends AppCompatActivity {
         alarmManager.cancel(pendingIntent);
         Toast.makeText(getApplicationContext(), "Alarm is Canceled", Toast.LENGTH_SHORT).show();
 
-        AlarmReceiver.getMediaPlayer().stop();
+        MediaPlayer mp = AlarmReceiver.getMediaPlayer();
+        if (mp != null) {
+            AlarmReceiver.getMediaPlayer().stop();
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/aalarm/Alarm.java
@@ -0,0 +1,63 @@
+package com.example.aalarm;
+
+public class Alarm {
+    private int year = -1;
+    private int month = -1;
+    private int day = -1;
+    private int hour = -1;
+    private int minute = -1;
+    private String game;
+
+    public Alarm(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public Alarm(int hour, int minute) {
+        this.hour = hour;
+        this.minute = minute;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public int getYear() {
+        return year;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+    public int getMonth() {
+        return month;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+    public int getDay() {
+        return day;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+    public int getHour() {
+        return hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+    public String getGame() {
+        return game;
+    }
+}
